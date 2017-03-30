41b7289ad6445a158d010666c104afe10a31df28
Binakot <binakot@gmail.com>
2017-03-30 18:48:59 +0300

    Solved #3 java problem.
    Next Hamming exercise.

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,6 +1,24 @@
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
+
 final class PangramChecker {
 
+    private static final int ENGLISH_ALPHABET_CHARACTERS_COUNT = 26;
+
     boolean isPangram(final String input) {
-        throw new UnsupportedOperationException("Method has not been implemented yet.");
+        if (input.isEmpty()) {
+            return false;
+        }
+
+        final byte[] chars = input.toLowerCase().getBytes(StandardCharsets.US_ASCII);
+        final Set<Byte> charSet = new HashSet<>(0);
+        for (final byte ch : chars) {
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+                charSet.add(ch);
+            }
+        }
+
+        return charSet.size() == ENGLISH_ALPHABET_CHARACTERS_COUNT;
     }
 }
