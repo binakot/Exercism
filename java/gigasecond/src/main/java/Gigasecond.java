a814548919a31f39d1d8462a66f7bb88e68e06da
Ivan Muratov <binakot@gmail.com>
2018-09-08 20:41:32 +0300

    Java - Gigasecond - https://exercism.io/my/solutions/094689d0eb214a50ac1ff2fa7df334d3

---
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+class Gigasecond {
+
+    private final LocalDateTime gigadate;
+
+    Gigasecond(LocalDate birthDate) {
+        this(birthDate.atStartOfDay());
+    }
+
+    Gigasecond(LocalDateTime birthDateTime) {
+        gigadate = birthDateTime.plusSeconds(1_000_000_000);
+    }
+
+    LocalDateTime getDate() {
+        return gigadate;
+    }
+}
